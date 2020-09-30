95167e27fa499e5ed7e3f45fd97a53001ae4505e
vaibhav <appautomator@gmail.com>
2020-09-30 11:27:30 +0100

    Added Steps to push the file from local to remote server

---
+++ MyProject/src/myPackage/Push.java
@@ -0,0 +1,21 @@
+package myPackage;
+
+public class Push {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/* This file tells you that how to push a file 
+		 * from local to remote server
+		 * Step1:Create a normal java project,package and a class
+		 * Step 2: Now right click on project -->Team--> Push to branch master
+           preview and provide login details and finally push
+           then open GIT
+           Window-Perspective--Other--Git--Click open
+           It will open a new git window
+           -->Remote Section--click on Push
+           */
+
+	}
+
+}
